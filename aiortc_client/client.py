6439007ed965ad334005d69de82a01fc4210b9f5
Sacha Perdrizat <sacha.perdrizat@gmail.com>
2021-06-27 20:02:04 +0200

    add logger lib and telegram support

--- aiortc_client/client.py
+++ aiortc_client/client.py
@@ -2,7 +2,9 @@ import asyncio
 import json
 import secrets
 import socketio
-import logging
+import logger
+
+from telegram import bot
 
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiortc.contrib.media import MediaPlayer, MediaRelay
@@ -13,25 +15,16 @@ sio = socketio.AsyncClient(ssl_verify=False)
 
 MAX_RND = 512
 
-HOST = 'https://192.168.1.108'
+HOST = 'https://<Signaling_Server_IP>'
 PORT = 443
 
-LOG = logging.getLogger("camera")
+BOT_TOKEN = "Your bot token here"
+CHAT_ID = 0# Your Chat ID here
 
-LOG.setLevel(logging.DEBUG)
+LOG = logger.get_logger()
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+#tg = bot.Bot(BOT_TOKEN)
 
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-LOG.addHandler(ch)
 
 # Reception des messages asynchrones
 def receiver_queue(signaling, messages):
@@ -40,9 +33,10 @@ def receiver_queue(signaling, messages):
         signaling.on(signal, lambda content, signal=signal: queue.put_nowait((signal, content)))
     return queue
 
+
 async def main(server):
     input("Press Enter to continue...")
-
+    pc = None
     await sio.connect(server)
     LOG.info("connected to my server")
     LOG.info("sending test message")
@@ -50,37 +44,39 @@ async def main(server):
     room_name = "room_pi{}".format(secrets.randbelow(MAX_RND))
 
     queue = receiver_queue(sio, messages=["joined", "created", "full", "invite", "ice_candidate", "bye", "new_peer"])
-    
-    await sio.emit('join',room_name)
 
-    while True :
+    await sio.emit('join', room_name)
+
+    while True:
         LOG.info("get signal from queue")
-        message = await queue.get() 
-        
-        LOG.debug(message)
-        
-        if message[0] == "created" :
-            LOG.info(f"room {message[1]} has been created")
-            LOG.info(f"Browse your navigator to {server}?room={message[1]} ")
+        message = await queue.get()
 
-        if message[0] in ("full", "joined") : 
+        LOG.debug(message)
+
+        if message[0] == "created":
+            url = f"{server}?room={message[1]}"
+            LOG.info(f"room {message[1]} has been created")
+            LOG.info(f"Browse your navigator to {url}")
+            #tg.send_message(chat_id=CHAT_ID, text="You can see your camera here {url}")
+
+        if message[0] in ("full", "joined"):
             LOG.info("error : room already exist")
 
-        if message[0] == "new_peer" : 
+        if message[0] == "new_peer":
             LOG.info("a new peer has entered the room")
 
-        if message[0] == "invite" : 
+        if message[0] == "invite":
             LOG.info("received an invite")
 
-            remoteOffer = RTCSessionDescription(sdp=message[1]['sdp'],type=message[1]['type']) 
+            remoteOffer = RTCSessionDescription(sdp=message[1]['sdp'], type=message[1]['type'])
             pc = RTCPeerConnection()
-            
+
             await pc.setRemoteDescription(remoteOffer)
             LOG.info("remote description set")
-            
+
             # Aquire the media device
             video_player = MediaPlayer('/dev/video0', format='v4l2', options=options_video)
-            audio_player = MediaPlayer("default", format="pulse")  
+            audio_player = MediaPlayer("default", format="pulse")
             LOG.info("Media aquired")
 
             for t in pc.getTransceivers():
@@ -103,29 +99,28 @@ async def main(server):
             # LOG.debug(answer)
 
             answer_json = json.dumps({
-                "sdp":answer.sdp,
-                "type":answer.type
+                "sdp": answer.sdp,
+                "type": answer.type
             })
 
-            await sio.emit("ok",answer_json)
-        
-        
-        if message[0] == "bye" :
+            await sio.emit("ok", answer_json)
+
+        if message[0] == "bye":
             LOG.info("End of the call")
-            await sio.emit("bye",None)
-            
+            await sio.emit("bye", None)
+
             # Cleanup
-            await pc.close()
+            if pc:
+                await pc.close()
             pc = None
             video_player = None
             video_player = None
             exit()
-             
-            
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     server = HOST + ":" + str(PORT)
 
     LOG.info(f"connecting to {server}")
-    
+
     asyncio.run(main(server))
